c35caa4f0f9e1ef7e1b8eca992574dd778689d78
Marcin <gasiorek.mail@gmail.com>
2023-08-08 21:03:10 +0200

    Initial commit

---
+++ src/main/java/pl/marcindev/miniUniverisityApp/MiniUniverisityAppApplication.java
@@ -0,0 +1,13 @@
+package pl.marcindev.miniUniverisityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUniverisityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniUniverisityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcindev/miniUniverisityApp/MiniUniverisityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcindev.miniUniverisityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniUniverisityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
